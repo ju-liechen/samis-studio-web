6fc5c8dcea7125fb4d0eba4ebb2e95ae676d999d
ju-liechen <juliechen4649@gmail.com>
2024-01-02 14:45:52 -0400

    Add footer

--- components/layouts/basic-layout.js
+++ components/layouts/basic-layout.js
@@ -1,10 +1,12 @@
 import { Header } from 'components/header'
+import { Footer } from 'components/footer'
 
 export const BasicLayout = ({ children }) => {
   return (
     <>
       <Header />
       {children}
+      <Footer />
     </>
   )
 }
