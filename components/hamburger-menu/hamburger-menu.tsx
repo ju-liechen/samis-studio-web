ea396ca8d4545603c49f6732ed2e756f282030c0
ju-liechen <juliechen4649@gmail.com>
2024-01-04 14:53:50 -0400

    Add mobile hamburger menu

---
+++ components/hamburger-menu/hamburger-menu.tsx
@@ -0,0 +1,71 @@
+import React, { useState } from 'react';
+import { useRouter } from 'next/router'
+import { motion } from 'framer-motion'
+import styles from './hamburger-menu.module.scss'
+import { HeaderButton } from 'components/header-button';
+
+export const HamburgerMenu = ({ }) => {
+  const router = useRouter()
+  const currentPage = router.pathname
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const menuBarVariants = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+    exit: { opacity: 0 }
+  };
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+        <span className={styles.line}></span>
+      </div>
+      {isMenuOpen && (
+        <motion.div
+        key="menuBar"
+        variants={menuBarVariants}
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        transition={{ duration: 1 }}
+        className={styles.menuBar}>
+          <HeaderButton
+            text="PRODUCTS"
+            isBold={true}
+            destination="/products"
+            currentPage={currentPage}
+            onClick={() => router.push('/products')}
+          />
+          <HeaderButton
+            text="COMMISSIONS"
+            isBold={true}
+            destination="/commissions"
+            currentPage={currentPage}
+            onClick={() => router.push('/commissions')}
+          />
+          <HeaderButton
+            text="ABOUT"
+            isBold={true}
+            destination="/about"
+            currentPage={currentPage}
+            onClick={() => router.push('/about')}
+          />
+          <HeaderButton
+            text="FAQ"
+            isBold={true}
+            destination="/faq"
+            currentPage={currentPage}
+            onClick={() => router.push('/faq')}
+            />
+          <button className={styles.closeButton} onClick={toggleMenu}><p>Close</p></button>
+        </motion.div>
+      )}
+    </div>
+  )
+}
--- components/header/header.tsx
+++ components/header/header.tsx
@@ -1,4 +1,6 @@
+import { HamburgerMenu } from 'components/hamburger-menu'
 import { HeaderButton } from 'components/header-button'
+import { useMediaQuery } from 'react-responsive'
 import { useRouter } from 'next/router'
 import Image from 'next/image'
 
@@ -8,10 +10,11 @@ import React from 'react'
 export const Header = () => {
   const router = useRouter()
   const currentPage = router.pathname
+  const isMobile = useMediaQuery({ maxWidth: 768 })
+  const isTablet = useMediaQuery({ maxWidth: 1024, minWidth: 768 })
 
   return (
     <div className={styles.header}>
-      <div className="container">
         <div className={styles.logo} onClick={() => router.push('/products')}>
           <Image
             src="/images/sami.png"
@@ -19,37 +22,44 @@ export const Header = () => {
             height={200}
             alt="sami sleep"
           />
-          Sami's Studio
-        </div>
-        <HeaderButton
-          text="PRODUCTS"
-          isBold={true}
-          destination="/products"
-          currentPage={currentPage}
-          onClick={() => router.push('/products')}
-        />
-        <HeaderButton
-          text="COMMISSIONS"
-          isBold={true}
-          destination="/commissions"
-          currentPage={currentPage}
-          onClick={() => router.push('/commissions')}
-        />
-        <HeaderButton
-          text="ABOUT"
-          isBold={true}
-          destination="/about"
-          currentPage={currentPage}
-          onClick={() => router.push('/about')}
-        />
-        <HeaderButton
-          text="FAQ"
-          isBold={true}
-          destination="/faq"
-          currentPage={currentPage}
-          onClick={() => router.push('/faq')}
-        />
+          <p className={styles.logoText}>Sami's Studio</p>
       </div>
+      <div className={`${styles.buttons} ${isMobile ? styles['buttons-mobile'] : ''} ${isTablet ? styles['buttons-tablet'] : ''}`}>
+      {isMobile ? (
+          <HamburgerMenu />
+        ) : (
+          <>
+            <HeaderButton
+              text="PRODUCTS"
+              isBold={true}
+              destination="/products"
+              currentPage={currentPage}
+              onClick={() => router.push('/products')}
+            />
+            <HeaderButton
+              text="COMMISSIONS"
+              isBold={true}
+              destination="/commissions"
+              currentPage={currentPage}
+              onClick={() => router.push('/commissions')}
+            />
+            <HeaderButton
+              text="ABOUT"
+              isBold={true}
+              destination="/about"
+              currentPage={currentPage}
+              onClick={() => router.push('/about')}
+            />
+            <HeaderButton
+              text="FAQ"
+              isBold={true}
+              destination="/faq"
+              currentPage={currentPage}
+              onClick={() => router.push('/faq')}
+              />
+          </>
+        )}
+        </div>
     </div>
   )
 }
